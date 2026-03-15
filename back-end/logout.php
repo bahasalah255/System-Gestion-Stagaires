a03155c835483a41598565a736c353d83eb1d9a1
salah <salaheddinebaha32@gmail.com>
2026-03-15 03:10:35 +0000

    Add logout functionality; update user token and set is_connected status

---
+++ back-end/logout.php
@@ -0,0 +1,21 @@
+<?php
+include 'connexion.php';
+include 'auth.php';
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type,Authorization");
+header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+$data = json_decode(file_get_contents("php://input"), true);
+$id = $data['id'];
+$stmt = $connexion->prepare('UPDATE user set token = ? , is_connected = ? where id_user = ?');
+$stmt->execute([null,0,$id]);
+
+
+
+?>
